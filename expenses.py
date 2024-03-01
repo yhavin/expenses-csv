dcb15679918013c36e9c635709b2c900399b8241
yhavin <y.havin@gmail.com>
2024-02-29 23:50:37 -0500

    Update foreign key constraints

--- expenses.py
+++ expenses.py
@@ -24,76 +24,92 @@ DEFAULT_CATEGORIES = [
 
 
 def init_db():
-    with sqlite3.connect(DB) as conn:
-        c = conn.cursor()
-        c.execute(
-            """
-            CREATE TABLE IF NOT EXISTS expense (
-                id INTEGER PRIMARY KEY,
-                date TEXT NOT NULL,
-                description TEXT NOT NULL,
-                category TEXT NOT NULL,
-                amount REAL NOT NULL
-            );
-            """
-        )
-        c.execute(
-            """
-            CREATE TABLE IF NOT EXISTS category (
-                id INTEGER PRIMARY KEY,
-                name TEXT UNIQUE NOT NULL
-            );
-            """
-        )
-        c.executemany(
-            "INSERT OR IGNORE into category (name) VALUES (?)",
-            DEFAULT_CATEGORIES
-        )
-        conn.commit()
+    """Create database tables if they do not exist."""
+    c = conn.cursor()
+    c.execute(
+        """
+        CREATE TABLE IF NOT EXISTS category (
+            id INTEGER PRIMARY KEY,
+            name TEXT UNIQUE NOT NULL
+        );
+        """
+    )
+    c.executemany(
+        "INSERT OR IGNORE into category (name) VALUES (?)",
+        DEFAULT_CATEGORIES
+    )
+    c.execute(
+        """
+        CREATE TABLE IF NOT EXISTS expense (
+            id INTEGER PRIMARY KEY,
+            date TEXT NOT NULL,
+            description TEXT NOT NULL,
+            category TEXT NOT NULL,
+            amount REAL NOT NULL,
+            FOREIGN KEY (category) REFERENCES category (name)
+        );
+        """
+    )
+    conn.commit()
 
 
-def autocomplete_categories(incomplete):
-    with sqlite3.connect(DB) as conn:
-        c = conn.cursor()
-        # Get all categories in single query since categories are not assumed to be high quantity
-        c.execute(
-            "SELECT * FROM category"
-        )
-        categories = c.fetchall()
-    return [category[0] for category in categories if category[0].startswith(incomplete)]
+def create_category(category):
+    """Create record in category table."""
+    c = conn.cursor()
+    c.execute(
+        "INSERT INTO category (name) VALUES (?)",
+        (category,)
+    )
+    conn.commit()
+    console.print(f"Category '{category}' created.")
+
+
+def validate_category(category):
+    """Return True if a given category record exists."""
+    c = conn.cursor()
+    c.execute(
+        "SELECT id FROM category WHERE name = ?",
+        (category,)
+    )
+    return c.fetchone() is not None
 
 
 @app.command()
 def add(
     date: str = typer.Option(datetime.now().strftime("%Y-%m-%d"), prompt="Date"),
     description: str = typer.Option(..., prompt="Description"),
-    category: str = typer.Option(..., prompt="Category", autocompletion=autocomplete_categories),
+    category: str = typer.Option(..., prompt="Category"),
     amount: float = typer.Option(..., prompt="Amount")
 ):
     """Add a new expense."""
     date = parser.parse(date).strftime("%Y-%m-%d")
-    with sqlite3.connect(DB) as conn:
-        c = conn.cursor()
-        c.execute(
-            "INSERT INTO expense (date, description, category, amount) VALUES (?, ?, ?, ?)", 
-            (date, description, category, amount)
-        )
-        conn.commit()
-    console.print("Expense added.")    
-
+    category = category.capitalize()
+    if not validate_category(category):
+        if typer.confirm(f"{category} category does not exist. Create it?"):
+            create_category(category)
+        else:
+            raise typer.Abort()
+    
+    c = conn.cursor()
+    c.execute(
+        "INSERT INTO expense (date, description, category, amount) VALUES (?, ?, ?, ?)", 
+        (date, description, category, amount)
+    )
+    conn.commit()
+    console.print("Expense added.")
+        
 
 @app.command()
 def list(page: int = typer.Option(1)):
     """List expenses with pagination."""
     items_per_page = 15
     offset = (page - 1) * items_per_page
-    with sqlite3.connect(DB) as conn:
-        c = conn.cursor()
-        c.execute(
-            "SELECT date, description, category, amount FROM expense ORDER BY date DESC LIMIT ? OFFSET ?",
-            (items_per_page, offset)
-        )
-        expenses = c.fetchall()
+    c = conn.cursor()
+    c.execute(
+        "SELECT date, description, category, amount FROM expense ORDER BY date DESC LIMIT ? OFFSET ?",
+        (items_per_page, offset)
+    )
+    expenses = c.fetchall()
     
     table = Table(show_header=True, header_style="bold blue")
     table.add_column("Date")
@@ -109,6 +125,7 @@ def list(page: int = typer.Option(1)):
 
 
 if __name__ == "__main__":
-    init_db()
-    # app()
-    typer.run(app)
+    with sqlite3.connect(DB) as conn:
+        conn.execute("PRAGMA foreign_keys = ON")
+        init_db()
+        app()
